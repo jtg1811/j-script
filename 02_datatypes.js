a1350be7498fc21dee51a6059662cff48cb240f5
jtg1811 <jeetgandhi1804@gmail.com>
2025-08-01 14:40:36 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; 
+
+// alert("Welcome to JavaScript Data Types!");    -> node.js , not browser
+
+let name = "jeet"  // string
+let age  = 25   // number
+let isLoggedIn = true   // boolean
