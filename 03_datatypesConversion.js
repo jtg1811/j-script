24180eb5b712159e2a84ae70417b09567a9ac825
jtg1811 <jeetgandhi1804@gmail.com>
2025-08-01 15:00:57 +0530

    boolean conversion

--- 03_datatypesConversion.js
+++ 03_datatypesConversion.js
@@ -1,16 +1,22 @@
-let score = "33"
-console.log(typeof score) // string
-valueNumber = Number(score)  // convert to number
-console.log(typeof valueNumber) // number
-console.log(valueNumber) // 33
+// let score = "33"
+// console.log(typeof score) // string
+// valueNumber = Number(score)  // convert to number
+// console.log(typeof valueNumber) // number
+// console.log(valueNumber) // 33
 
-let score1 = "33abc"
-console.log(typeof Number(score1)) // still number
+// let score1 = "33abc"
+// console.log(typeof Number(score1)) // still number
 
-// so need to be careful with conversion if its really a number or not
-console.log(Number(score1)) // NaN (Not a Number)
+// // so need to be careful with conversion if its really a number or not
+// console.log(Number(score1)) // NaN (Not a Number)
 
-// if score value is null , it will be converted to 0
-// if score value is undefined , it will be converted to NaN
-// if score value is true , it will be converted to 1
-// if score value is false , it will be converted to 0
+// // if score value is null , it will be converted to 0
+// // if score value is undefined , it will be converted to NaN
+// // if score value is true , it will be converted to 1
+// // if score value is false , it will be converted to 0
+
+let isLoggedIn = 1
+let isLoggedInNumber = Boolean(isLoggedIn) // convert to boolean
+console.log(isLoggedInNumber) // true
+// if isLoggedIn is 0 , it will be converted to false
+// if isLoggedIn is empty string , it will be converted to false
