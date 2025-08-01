84ef378c4767f540cf67913a8ec4d604953f0aca
jtg1811 <jeetgandhi1804@gmail.com>
2025-08-01 17:56:23 +0530

    operations

---
+++ conversionOperations.js
@@ -0,0 +1,13 @@
+console.log(1+2);
+console.log(1+2+"2");
+console.log("1"+2+2);
+
+// whichever enters first that data type will be used
+
+let str1 = "jeet";
+let str2 = " gandhi";
+console.log(str1 + str2);  // jeet gandhi.  -> string concatenation
+
+gameCounter = 0 
+gameCounter++      // 1
+console.log(gameCounter)  
