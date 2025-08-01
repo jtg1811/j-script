fef3ee453405fc6f9b60b5e46aeffdd303cf20be
jtg1811 <jeetgandhi1804@gmail.com>
2025-08-01 14:26:16 +0530

    Renamed variable.js to 01_variable.js

--- 01_variable.js
+++ 01_variable.js
@@ -3,6 +3,7 @@ let accountnName = 'Jeet'
 var accountEmail = 'jeet@123'
 accountCity = 'Mumbai'
 
+
 // accountId = 54321.  cant cange const variable
 accountnName = 'Gandhi' // can change let variable
 accountEmail = 'jeet@456' // can change var variable
