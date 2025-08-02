6b683b63bbc820651baa6478e79ce7668aa09d59
jtg1811 <jeetgandhi1804@gmail.com>
2025-08-02 17:53:41 +0530

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -2,3 +2,25 @@ function sayHello(){
     console.log("hello function")
 }
 sayHello()
+
+function addNumbers(num1,num2){
+    let result = num1 + num2 
+    return result
+}
+const result = addNumbers(3,6)
+console.log(`result: ${result}`)
+
+function loginUser(username){
+    if (username === undefined){
+        return "please enter username"
+    }
+    return `${username} just logged in`
+}
+console.log(loginUser("jeet"))
+
+function loginUser(username="jeet"){      // default value
+    if (username === undefined){
+        return "please enter username"
+    }
+    return `${username} just logged in`
+}
