2652c4a63e50a76358c29f5f63c6ee4a6b43c58f
jtg1811 <jeetgandhi1804@gmail.com>
2025-08-02 21:59:20 +0530

    some imp concept based on this.

---
+++ 03_basics/04_arrow.js
@@ -0,0 +1,17 @@
+// this.something... cant be use inside normal function but it can be work inside the objects
+
+const user = {
+    name : "jeet",
+    age : 30,
+    welcomeMessage : function welcome(){
+        console.log("welcome....")
+        console.log(this)
+    }
+}
+user.welcomeMessage()
+console.log(this)
+
+// function hello(){
+//     console.log(this)
+// }
+// hello()
