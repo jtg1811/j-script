49df296ce12154176f125180b75447b6e61be657
jtg1811 <jeetgandhi1804@gmail.com>
2025-08-02 08:39:31 +0530

    numbers and math

---
+++ 07_numbers_and_maths.js
@@ -0,0 +1,27 @@
+// const score = 100
+// console.log(score); // 100
+
+// const balance = new Number(100);
+// console.log(balance); // [Number: 100]
+
+// console.log(balance.toString())
+// console.log(balance.toFixed(2)) // 100.00
+
+// const otherNumber = 100.183456789;
+// console.log(otherNumber.toPrecision(4)) // 100.0
+
+// console.log(Math);
+
+// console.log(Math.abs(-10)); // 10
+// console.log(Math.ceil(10.1)); // 11
+// console.log(Math.floor(10.9)); // 10
+// console.log(Math.round(10.5)); // 11
+// console.log(Math.max(10, 20, 30)); // 30
+// console.log(Math.min(10, 20, 30)); // 10
+
+
+console.log(Math.random()) // random number between 0 and 1
+console.log((Math.random() * 10)+1) // random number between 0 and 10
+const max = 10
+const min = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) // random number between 10 and 20
