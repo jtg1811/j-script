e342935e96608d904689ecd6f0ae9b60ac095d87
jtg1811 <jeetgandhi1804@gmail.com>
2025-08-03 12:38:25 +0530

    for of concept

---
+++ 05_iterations/three.js
@@ -0,0 +1,26 @@
+// for of
+
+const arr = ["jeet","zeel","meet","manthan"]
+
+for(let val of arr) {
+  console.log(val)
+}
+
+const arr1 = [
+    {name:"jeet",id:1},
+    {name:"manthan",id:2},
+    {name:"zeel",id:3},
+    {name:"meet",id:4}
+]
+
+for(let val of arr1) {
+    console.log(val.name)
+    console.log(val.id)
+}
+
+for(let val of arr1){
+    for(let key in val){
+        console.log(`${key} : ${val[key]}`)
+    }
+}
+
