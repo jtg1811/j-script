b05bf420d115f8c4aa95b112e712391af202b06a
jtg1811 <jeetgandhi1804@gmail.com>
2025-08-03 12:01:05 +0530

    forloop concept

---
+++ 05_iterations/one.js
@@ -0,0 +1,20 @@
+// for loop
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element)
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop : ${i}`)
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(i + ' * ' + j + ' = ' + i*j);
+//     }
+// }
+
+let heroes = ["thor","ironman","hulk"]
+for (let i = 0; i < heroes.length; i++) {
+    const element = heroes[i];
+    console.log(element)
+}
