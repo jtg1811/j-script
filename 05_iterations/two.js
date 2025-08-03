7cbdfaa4959d5c59be2773545109a684b8e262b6
jtg1811 <jeetgandhi1804@gmail.com>
2025-08-03 12:39:04 +0530

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+// while loop
+
+let index = 0
+while (index <= 10) {
+    console.log(`index value: ${index}`)
+    index = index + 2    
+}
+
+let heroes = ["thor","hulk","ironman"]
+let val = 0
+while(val < heroes.length){
+    console.log(heroes[val])
+    val = val + 1
+}
+
+// do 
+let score = 11
+do {
+    console.log(`score: ${score}`)
+    score += 1
+} while (score < 10);
