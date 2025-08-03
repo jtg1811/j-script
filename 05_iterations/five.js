3b02818268da56f3585ec570322b73df43b5436f
jtg1811 <jeetgandhi1804@gmail.com>
2025-08-03 14:43:30 +0530

    imp foreach concepts

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -10,8 +10,37 @@ const coding = ["js","py","cpp","c"]
 //     console.log(item)
 // })
 
-function printMe(item){
-    console.log(item)
+// function printMe(item){
+//     console.log(item)
+// }
+
+// coding.forEach(printMe)
+
+coding.forEach((item,index,arr) => {
+    console.log(item,index,arr)
+})
+
+const myCoding = [
+    {
+        programLanguage : "JavaScript",
+        languageFile : "JS"
+    },
+    {
+        programLanguage : "python",
+        languageFile : "py"
+    },
+    {
+        programLanguage : "java",
+        languageFile : "java"
+    }
+]
+
+for(const code of myCoding){
+    for(const key in code){
+        console.log(`${key} :   ${code[key]}`)
+    }
 }
 
-coding.forEach(printMe)
+myCoding.forEach((item) => {
+    console.log(item.programLanguage)
+})
