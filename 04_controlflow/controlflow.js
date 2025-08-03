d40ef1e28495159731429599b77dc154e6805ba3
jtg1811 <jeetgandhi1804@gmail.com>
2025-08-03 11:41:44 +0530

    nullish colalescing operator

--- 04_controlflow/controlflow.js
+++ 04_controlflow/controlflow.js
@@ -1,16 +1,24 @@
 // to check for empty array and empty object
 
-const myarr = []
-if (myarr.length === 0){
-    console.log("array is empty")
-}
+// const myarr = []
+// if (myarr.length === 0){
+//     console.log("array is empty")
+// }
 
-const myobj = {}
-if (Object.keys(myobj).length === 0){
-    console.log("obj is empty")
-}
+// const myobj = {}
+// if (Object.keys(myobj).length === 0){
+//     console.log("obj is empty")
+// }
 
-if(""){
-    console.log(true)
-}
-console.log(false)
+// if(""){
+//     console.log(true)
+// }
+// console.log(false)
+
+// nullish colalescing operator ( ?? ) : null defined
+
+let val,val1 ;
+val = 6 ?? 10    // 6
+val1 = null ?? 10  // 10 
+
+console.log(val1)
