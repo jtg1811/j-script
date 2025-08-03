8ff01d5181fc295b64b0759822440ebef5a5b5d0
jtg1811 <jeetgandhi1804@gmail.com>
2025-08-03 11:35:33 +0530

    control flow

---
+++ 04_controlflow/controlflow.js
@@ -0,0 +1,11 @@
+// to check for empty array and empty object
+
+const myarr = []
+if (myarr.length === 0){
+    console.log("array is empty")
+}
+
+const myobj = {}
+if (Object.keys(myobj).length === 0){
+    console.log("obj is empty")
+}
